25529e0b42f4e6b054b3c353d464618f64512cb7
2a6m <maxime.bourguignon1996@gmail.com>
2019-06-12 19:31:06 +0200

    create bullets

--- app.js
+++ app.js
@@ -18,6 +18,26 @@ App = function()
         wade.addSceneObject(ship);
     };
 
+    wade.setMainLoopCallback(function()
+    {
+        // code to execute several times per second
+        // (time % rate == 0)
+        if (wade.isMouseDown())
+        {
+            var shipPosition = ship.getPosition();
+            var shipSize = ship.getSprite().getSize();
+            var sprite = new Sprite('images/bullet-open-source.png');
+            var bullet = new SceneObject(sprite, 0, shipPosition.x, shipPosition.y - shipSize.y / 2);
+            wade.addSceneObject(bullet);
+            bullet.moveTo(shipPosition.x, -500, 600);
+
+            bullet.onMoveComplete = function()
+            {
+                wade.removeSceneObject(this);
+            };
+        }
+    }, 'fire');
+
     this.onMouseMove = function(eventData)
     {
         ship.setPosition(eventData.screenPosition.x, eventData.screenPosition.y);
