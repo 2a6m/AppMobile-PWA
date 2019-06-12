c66b3aa746451c5359098b90452b32d330d9077e
2a6m <maxime.bourguignon1996@gmail.com>
2019-06-12 18:42:04 +0200

    create ship

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 App = function()
 {
+    var ship;
+
+    this.load  =function()
+    {
+        wade.loadImage('images/ship-tux.png');
+    };
+
     this.init = function()
     {
+        wade.setMinScreenSize(398,708);
+        wade.setMaxScreenSize(1920,1080);
 
+        var sprite = new Sprite('images/ship-tux.png');
+        var mousePosition = wade.getMousePosition();
+        ship = new SceneObject(sprite, 0, mousePosition.x, mousePosition.y);
+        wade.addSceneObject(ship);
+    };
+
+    this.onMouseMove = function(eventData)
+    {
+        ship.setPosition(eventData.screenPosition.x, eventData.screenPosition.y);
     };
 };
